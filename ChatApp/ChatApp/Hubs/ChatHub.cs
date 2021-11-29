4b4fc7e354fb28e79b27063278b6ba7e7dcd5261
Lê Công Đức <lecongduc.1999@gmail.com>
2021-11-29 23:14:25 +0700

    .

---
+++ ChatApp/ChatApp/Hubs/ChatHub.cs
@@ -0,0 +1,73 @@
+﻿using ChatApp.Models;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatApp
+{
+    public class ChatHub : Hub
+    {
+        public static HashSet<Users> connectionList = new HashSet<Users>();
+
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.All.SendAsync("OnConnectedAsync", $"{Context.ConnectionId} joined", Context.ConnectionId);
+        }
+
+        public override async Task OnDisconnectedAsync(Exception ex)
+        {
+            await Clients.Others.SendAsync("OnDisconnectedAsync", $"{Context.ConnectionId} left");
+        }
+
+        public Task Send(string message)
+        {
+            Users us = new Users();
+            us.ConnectionId = Context.ConnectionId;
+            us.Username = message;
+            connectionList.Add(us);
+            return Clients.All.SendAsync("Send", message, connectionList);
+        }
+
+        public Task SendToOthers(string message)
+        {
+            return Clients.Others.SendAsync("SendToOthers", $"{Context.ConnectionId}: {message}");
+        }
+
+        public Task SendToConnection(string connectionId, string username, string message)
+        {
+            return Clients.Client(connectionId).SendAsync("SendToConnection", Context.ConnectionId, username, message);
+        }
+
+        public Task SendToGroup(string groupName, string username, string message)
+        {
+            return Clients.Group(groupName).SendAsync("SendToGroup", groupName, username, message);
+            //return Clients.Group(groupName).SendAsync("SendToGroup", $"{Context.ConnectionId}@{groupName}: {message}");
+        }
+
+        public Task SendToOthersInGroup(string groupName, string message)
+        {
+            return Clients.OthersInGroup(groupName).SendAsync("SendToOthersInGroup", $"{Context.ConnectionId}@{groupName}: {message}");
+        }
+
+        public async Task JoinGroup(string groupName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+
+            await Clients.Group(groupName).SendAsync("JoinGroup", $"{Context.ConnectionId} joined {groupName}");
+        }
+
+        public async Task LeaveGroup(string groupName)
+        {
+            await Clients.Group(groupName).SendAsync("LeaveGroup", $"{Context.ConnectionId} left {groupName}");
+
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
+        }
+
+        public Task Echo(string message)
+        {
+            return Clients.Caller.SendAsync("Echo", $"{Context.ConnectionId}: {message}");
+        }
+    }
+}
---
+++ ChatApp/ChatApp/Models/Users.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatApp.Models
+{
+    public class Users
+    {
+        string _connectionId;
+        string _username;
+
+        public string ConnectionId { get => _connectionId; set => _connectionId = value; }
+        public string Username { get => _username; set => _username = value; }
+    }
+}
